df88e4dc56184f807fbcc84eb755dd4f8d9e7441
Matt Moore <mattmoor@chainguard.dev>
2022-02-23 18:08:41 -0800

    Add gosec, move publish

--- cmd/apko/cli/publish.go
+++ cmd/apko/cli/publish.go
@@ -22,7 +22,6 @@ import (
 	"chainguard.dev/apko/pkg/build"
 	"chainguard.dev/apko/pkg/build/oci"
 	"chainguard.dev/apko/pkg/build/types"
-	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -40,18 +39,10 @@ in a keychain.`,
 		Example: `  apko publish <config.yaml> <tag>`,
 		Args:    cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			digest, err := PublishCmd(cmd.Context(), args[0], args[1])
+			err := PublishCmd(cmd.Context(), args[0], args[1], imageRefs)
 			if err != nil {
 				return err
 			}
-			if imageRefs != "" {
-				if err := os.WriteFile(imageRefs, []byte(digest.String()), 0666); err != nil {
-					return err
-				}
-			}
-			// TODO: We should write the image digest to STDOUT (and everything
-			// else to STDERR) in order to enable command composition
-			// e.g. kn service create --image=$(apko publish ...)
 			return nil
 		},
 	}
@@ -61,18 +52,18 @@ in a keychain.`,
 	return cmd
 }
 
-func PublishCmd(ctx context.Context, configFile string, imageRef string) (name.Digest, error) {
+func PublishCmd(ctx context.Context, configFile string, imageRef string, outputRefs string) error {
 	log.Printf("building image '%s' from config file '%s'", imageRef, configFile)
 
 	ic := types.ImageConfiguration{}
 	err := ic.Load(configFile)
 	if err != nil {
-		return name.Digest{}, errors.Wrap(err, "failed to load image configuration")
+		return errors.Wrap(err, "failed to load image configuration")
 	}
 
 	wd, err := os.MkdirTemp("", "apko-*")
 	if err != nil {
-		return name.Digest{}, errors.Wrap(err, "failed to create working directory")
+		return errors.Wrap(err, "failed to create working directory")
 	}
 	defer os.RemoveAll(wd)
 
@@ -83,14 +74,25 @@ func PublishCmd(ctx context.Context, configFile string, imageRef string) (name.D
 
 	layerTarGZ, err := bc.BuildLayer()
 	if err != nil {
-		return name.Digest{}, errors.Wrap(err, "failed to build layer image")
+		return errors.Wrap(err, "failed to build layer image")
 	}
 	defer os.Remove(layerTarGZ)
 
 	digest, err := oci.PublishImageFromLayer(imageRef, layerTarGZ, bc.ImageConfiguration)
 	if err != nil {
-		return name.Digest{}, errors.Wrap(err, "failed to build OCI image")
+		return errors.Wrap(err, "failed to build OCI image")
 	}
 
-	return digest, nil
+	// If provided, this is the name of the file to write digest referenced into
+	if outputRefs != "" {
+		//nolint:gosec // Make image ref file readable by non-root
+		if err := os.WriteFile(outputRefs, []byte(digest.String()), 0666); err != nil {
+			return err
+		}
+	}
+	// TODO: We should write the image digest to STDOUT (and everything
+	// else to STDERR) in order to enable command composition
+	// e.g. kn service create --image=$(apko publish ...)
+
+	return nil
 }
