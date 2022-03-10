4c96f71448b76fa69de2b1b0730126be17c91c79
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-03-09 22:43:44 -0600

    Add first iteration of SPDX generator
    This commit adds to the sbom package the first iteration of the SPDX
    generator. For now testing is insufficient and the data epxressed is
    fairly limited. But it now runs and produces valid SPDX output encoded
    in json.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ pkg/sbom/generator/spdx/spdx.go
@@ -0,0 +1,188 @@
+// Copyright 2022 Chainguard, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spdx
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/google/uuid"
+
+	"chainguard.dev/apko/pkg/sbom/options"
+	"chainguard.dev/apko/pkg/sbom/purl"
+)
+
+const NOASSERTION = "NOASSERTION"
+
+type SPDX struct{}
+
+func New() SPDX {
+	return SPDX{}
+}
+
+func (sx *SPDX) Key() string {
+	return "spdx"
+}
+
+func (sx *SPDX) Ext() string {
+	return "spdx.json"
+}
+
+// Generate writes a cyclondx sbom in path
+func (sx *SPDX) Generate(opts *options.Options, path string) error {
+	doc := &Document{
+		ID:      "SPDXRef-DOCUMENT",
+		Name:    "apko-" + uuid.NewString(),
+		Version: "SPDX-2.2",
+		CreationInfo: CreationInfo{
+			Created: "1970-01-01T00:00:00Z",
+			Creators: []string{
+				"Tool: apko (v0.0.0)",
+				"Organization: Chainguard, Inc",
+			},
+			LicenseListVersion: "3.16",
+		},
+		DataLicense:   "CC0-1.0",
+		Namespace:     "https://spdx.org/spdxdocs/apko-" + uuid.NewString(),
+		Packages:      []Package{},
+		Relationships: []Relationship{},
+	}
+
+	mainPackage := Package{
+		ID:               "SPDXRef-Package-apko-os-layer-" + uuid.NewString(),
+		Name:             "apko-OS-Layer",
+		Version:          opts.OS.Version,
+		FilesAnalyzed:    false,
+		LicenseConcluded: NOASSERTION,
+		LicenseDeclared:  NOASSERTION,
+		Description:      "",
+		DownloadLocation: NOASSERTION, // switch when https://gitlab.alpinelinux.org/alpine/go/-/merge_requests/4
+		Originator:       "",
+		SourceInfo:       "",
+		CopyrightText:    NOASSERTION,
+		Checksums:        []Checksum{},
+		ExternalRefs:     []ExternalRef{},
+	}
+
+	doc.Packages = append(doc.Packages, mainPackage)
+	doc.DocumentDescribes = []string{mainPackage.ID}
+
+	for _, pkg := range opts.Packages {
+		// add the package
+		p := Package{
+			ID:               "SPDXRef-Package-apko-pkg-" + uuid.NewString(),
+			Name:             pkg.Name,
+			Version:          pkg.Version,
+			FilesAnalyzed:    false,
+			LicenseConcluded: pkg.License,
+			LicenseDeclared:  NOASSERTION,
+			Description:      pkg.Description,
+			//DownloadLocation: pkg.,
+			Originator:    pkg.Maintainer,
+			SourceInfo:    "",
+			CopyrightText: NOASSERTION,
+			Checksums: []Checksum{
+				{
+					Algorithm: "SHA1",
+					Value:     fmt.Sprintf("%x", pkg.Checksum[1:]),
+				},
+			},
+			ExternalRefs: []ExternalRef{
+				{
+					Category: "PACKAGE_MANAGER",
+					Locator:  purl.Package(opts.OS.ID, pkg),
+					Type:     "purl",
+				},
+			},
+		}
+
+		doc.Packages = append(doc.Packages, p)
+
+		// Add to the relationships list
+		doc.Relationships = append(doc.Relationships, Relationship{
+			Element: mainPackage.ID,
+			Type:    "CONTAINS",
+			Related: p.ID,
+		})
+	}
+
+	out, err := os.Create(filepath.Join(path, fmt.Sprintf("%s.%s", opts.FileName, sx.Ext())))
+	if err != nil {
+		return fmt.Errorf("opening SBOM path %s for writing: %w", path, err)
+	}
+	defer out.Close()
+
+	enc := json.NewEncoder(out)
+	enc.SetIndent("", "  ")
+
+	if err := enc.Encode(doc); err != nil {
+		return fmt.Errorf("encoding spdx sbom: %w", err)
+	}
+
+	return nil
+}
+
+type Document struct {
+	ID                string         `json:"SPDXID"`
+	Name              string         `json:"name"`
+	Version           string         `json:"spdxVersion"`
+	CreationInfo      CreationInfo   `json:"creationInfo"`
+	DataLicense       string         `json:"dataLicense"`
+	Namespace         string         `json:"documentNamespace"`
+	DocumentDescribes []string       `json:"documentDescribes"`
+	Packages          []Package      `json:"packages"`
+	Relationships     []Relationship `json:"relationships"`
+}
+
+type CreationInfo struct {
+	Created            string   `json:"created"` // Date
+	Creators           []string `json:"creators"`
+	LicenseListVersion string   `json:"licenseListVersion"`
+}
+
+type Package struct {
+	ID               string        `json:"SPDXID"`
+	Name             string        `json:"name"`
+	Version          string        `json:"versionInfo"`
+	FilesAnalyzed    bool          `json:"filesAnalyzed"`
+	LicenseConcluded string        `json:"licenseConcluded"`
+	LicenseDeclared  string        `json:"licenseDeclared"`
+	Description      string        `json:"description"`
+	DownloadLocation string        `json:"downloadLocation"`
+	Originator       string        `json:"originator"`
+	SourceInfo       string        `json:"sourceInfo"`
+	CopyrightText    string        `json:"copyrightText"`
+	Checksums        []Checksum    `json:"checksums"`
+	ExternalRefs     []ExternalRef `json:"externalRefs"`
+}
+
+type Checksum struct {
+	Algorithm string `json:"algorithm"`
+	Value     string `json:"checksumValue"`
+}
+
+type ExternalRef struct {
+	Category string `json:"referenceCategory"`
+	Locator  string `json:"referenceLocator"`
+	Type     string `json:"referenceType"`
+}
+
+type Relationship struct {
+	Element string `json:"spdxElementId"`
+	Type    string `json:"relationshipType"`
+	Related string `json:"relatedSpdxElement"`
+}
