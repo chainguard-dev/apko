6429d10a7ebab856980cc7edb50da05c17f30b34
kruskal <99559985+kruskall@users.noreply.github.com>
2022-03-12 00:12:27 +0100

    lint: fix linter issues

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Chainguard, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package fs
 
 import (
@@ -7,8 +21,8 @@ import (
 )
 
 type rlfs struct {
-	base        string
-	f           fs.FS
+	base string
+	f    fs.FS
 }
 
 func (f *rlfs) Readlink(name string) (string, error) {
@@ -16,9 +30,9 @@ func (f *rlfs) Readlink(name string) (string, error) {
 }
 
 func (f *rlfs) Open(name string) (fs.File, error) {
-	return f.Open(name)
+	return f.f.Open(name)
 }
 
 func (f *rlfs) Stat(name string) (fs.FileInfo, error) {
-	return f.Stat(name)
+	return os.Stat(filepath.Join(f.base, name))
 }
--- pkg/tarball/append.go
+++ pkg/tarball/append.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Chainguard, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tarball
 
 import (
@@ -7,17 +21,17 @@ import (
 	"io/fs"
 )
 
-type multi struct {
+type MultiTar struct {
 	out io.Writer
 }
 
-func Out(dst io.Writer) *multi {
-	return &multi{
+func Out(dst io.Writer) *MultiTar {
+	return &MultiTar{
 		out: dst,
 	}
 }
 
-func (m *multi) Append(ctx *Context, src fs.FS, extra ...io.Writer) error {
+func (m *MultiTar) Append(ctx *Context, src fs.FS, extra ...io.Writer) error {
 	dst := io.MultiWriter(append([]io.Writer{m.out}, extra...)...)
 
 	gzw := gzip.NewWriter(dst)
@@ -29,7 +43,7 @@ func (m *multi) Append(ctx *Context, src fs.FS, extra ...io.Writer) error {
 	return ctx.writeTar(tw, src)
 }
 
-func (m *multi) Close() {
+func (m *MultiTar) Close() {
 	gzw := gzip.NewWriter(m.out)
 	defer gzw.Close()
 
