2a37e9857e3b291e1f9081c9c3873b5e69cd0591
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-07-05 18:50:07 -0500

    Only generate index sbom when building more than one image
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- internal/cli/publish.go
+++ internal/cli/publish.go
@@ -229,14 +229,16 @@ func PublishCmd(ctx context.Context, outputRefs string, archs []types.Architectu
 			}
 		}
 
-		if err := bc.GenerateIndexSBOM(finalDigest, imgs); err != nil {
-			return fmt.Errorf("generating index SBOM: %w", err)
-		}
+		if len(imgs) > 1 {
+			if err := bc.GenerateIndexSBOM(finalDigest, imgs); err != nil {
+				return fmt.Errorf("generating index SBOM: %w", err)
+			}
 
-		if _, err := oci.PostAttachSBOM(
-			idx, sbompath, bc.Options.SBOMFormats, types.Architecture{}, bc.Logger(), bc.Options.Tags...,
-		); err != nil {
-			return fmt.Errorf("attaching sboms to index: %w", err)
+			if _, err := oci.PostAttachSBOM(
+				idx, sbompath, bc.Options.SBOMFormats, types.Architecture{}, bc.Logger(), bc.Options.Tags...,
+			); err != nil {
+				return fmt.Errorf("attaching sboms to index: %w", err)
+			}
 		}
 	}
 
