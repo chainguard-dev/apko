a65dafcf924b9e38b6836002e39c0c46afa02820
Ariadne Conill <ariadne@chainguard.dev>
2022-07-18 13:36:55 +0000

    fetch: unexport parseRef and friends

--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -25,42 +25,42 @@ import (
 	"github.com/go-git/go-git/v5/plumbing"
 )
 
-type Resource struct {
-	Host       string
-	Repository string
-	Path       string
-	Reference  string
+type resource struct {
+	host       string
+	repository string
+	path       string
+	reference  string
 }
 
-func (r *Resource) URI() *url.URL {
+func (r *resource) uri() *url.URL {
 	return &url.URL{
 		Scheme: "https",
-		Host:   r.Host,
-		Path:   r.Repository,
+		Host:   r.host,
+		Path:   r.repository,
 	}
 }
 
-func (r *Resource) String() string {
-	baseRef := strings.Join([]string{r.Host, r.Repository, r.Path}, "/")
+func (r *resource) String() string {
+	baseRef := strings.Join([]string{r.host, r.repository, r.path}, "/")
 
-	if r.Reference != "" {
-		return fmt.Sprintf("%s@%s", baseRef, r.Reference)
+	if r.reference != "" {
+		return fmt.Sprintf("%s@%s", baseRef, r.reference)
 	}
 
 	return baseRef
 }
 
-func ParseRef(path string) (*Resource, error) {
+func parseRef(path string) (*resource, error) {
 	paths, referenceName, _ := strings.Cut(path, "@")
 	pathElements := strings.Split(paths, string(os.PathSeparator))
 
 	// TODO(kaniini): We presently assume a github-like forge for figuring out
 	// our paths.  Should come up with a better strategy at some point...
-	ref := Resource{
-		Host:       pathElements[0],
-		Repository: filepath.Join(pathElements[1:3]...),
-		Path:       filepath.Join(pathElements[3:]...),
-		Reference:  referenceName,
+	ref := resource{
+		host:       pathElements[0],
+		repository: filepath.Join(pathElements[1:3]...),
+		path:       filepath.Join(pathElements[3:]...),
+		reference:  referenceName,
 	}
 
 	return &ref, nil
@@ -73,13 +73,13 @@ func Fetch(path string) ([]byte, error) {
 	}
 	defer os.RemoveAll(tempDir)
 
-	resource, err := ParseRef(path)
+	resource, err := parseRef(path)
 	if err != nil {
 		return []byte{}, fmt.Errorf("failed to parse git reference %s: %w", path, err)
 	}
 
 	repo, err := git.PlainClone(tempDir, false, &git.CloneOptions{
-		URL: resource.URI().String(),
+		URL: resource.uri().String(),
 	})
 	if err != nil {
 		return []byte{}, fmt.Errorf("failed to clone %s: %w", resource.String(), err)
@@ -87,7 +87,7 @@ func Fetch(path string) ([]byte, error) {
 
 	var hash *plumbing.Hash
 
-	if resource.Reference == "" {
+	if resource.reference == "" {
 		ref, err := repo.Head()
 		if err != nil {
 			return []byte{}, fmt.Errorf("failed to fetch repository head: %w", err)
@@ -96,11 +96,11 @@ func Fetch(path string) ([]byte, error) {
 		refHash := ref.Hash()
 		hash = &refHash
 	} else {
-		rev := plumbing.Revision(resource.Reference)
+		rev := plumbing.Revision(resource.reference)
 
 		hash, err = repo.ResolveRevision(rev)
 		if err != nil {
-			return []byte{}, fmt.Errorf("failed to fetch repository rev %s: %w", resource.Reference, err)
+			return []byte{}, fmt.Errorf("failed to fetch repository rev %s: %w", resource.reference, err)
 		}
 	}
 
@@ -116,8 +116,7 @@ func Fetch(path string) ([]byte, error) {
 		return []byte{}, fmt.Errorf("failed to checkout %s: %w", *hash, err)
 	}
 
-	paths := append([]string{tempDir}, resource.Path)
-	target := filepath.Join(paths...)
+	target := filepath.Join(tempDir, resource.path)
 
 	data, err := os.ReadFile(target)
 	if err != nil {
