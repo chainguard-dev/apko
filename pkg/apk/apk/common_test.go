6947fe7002cfc78bb90215f5e298bf3d5e436eb0
Dimitri John Ledkov <dimitri.ledkov@chainguard.dev>
2024-05-11 11:38:46 +0100

    test: Add test for RSA256 signed APKINDEX
    This test case should fail, as apko has no support for RSA256
    signatures.
    
    And yet it passes.... as if the rsa256-signed/APKINDEX.tar.gz is not
    fetched nor verified.
    
    This in preparation to add support for RSA256 signed APK repositories.

--- pkg/apk/apk/common_test.go
+++ pkg/apk/apk/common_test.go
@@ -27,6 +27,7 @@ import (
 const (
 	testPrimaryPkgDir   = "testdata/alpine-316"
 	testAlternatePkgDir = "testdata/alpine-317"
+	testRSA256IndexPkgDir = "testdata/rsa256-signed"
 )
 
 type testLocalTransport struct {
