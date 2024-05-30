b268ea3834292082854203c93691f4e9e09090fd
Jason Hall <jason@chainguard.dev>
2024-05-30 00:05:51 -0400

    support basic HTTP auth
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/build/build_test.go
+++ pkg/build/build_test.go
@@ -16,6 +16,8 @@ package build_test
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"path/filepath"
 	"testing"
 
@@ -23,6 +25,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"chainguard.dev/apko/pkg/build"
+	"chainguard.dev/apko/pkg/build/types"
 )
 
 func TestBuildLayer(t *testing.T) {
@@ -101,4 +104,76 @@ func TestBuildImageFromTooOldResolvedFile(t *testing.T) {
 	require.Equal(t, "failed getting packages for install from lockfile testdata/apko.pre-0.13.lock.json: "+
 		"locked package pretend-baselayout has missing checksum (please regenerate the lock file with Apko >=0.13)",
 		err.Error())
+}
+
+func TestAuth_good(t *testing.T) {
+	called := false
+	testUser, testPass := "user", "pass"
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("ETag", "123")
+		if r.Method == http.MethodHead {
+			return
+		}
+
+		called = true
+		if gotuser, gotpass, ok := r.BasicAuth(); !ok || gotuser != testUser || gotpass != testPass {
+			t.Logf("got user: %q, pass: %q", gotuser, gotpass)
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		http.FileServer(http.Dir("testdata/packages")).ServeHTTP(w, r)
+	}))
+	defer s.Close()
+
+	ctx := context.Background()
+	bc, err := build.New(ctx, fs.NewMemFS(),
+		build.WithAuth(testUser, testPass),
+		build.WithImageConfiguration(types.ImageConfiguration{
+			Contents: types.ImageContents{
+				Repositories: []string{s.URL},
+				Keyring:      []string{s.URL + "/melange.rsa.pub"},
+				Packages:     []string{"pretend-baselayout"},
+			},
+			Archs: types.ParseArchitectures([]string{"amd64", "arm64"}),
+		}),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = bc.BuildImage(ctx)
+	require.NoError(t, err, "build image failed")
+	require.True(t, called)
+}
+
+func TestAuth_bad(t *testing.T) {
+	called := false
+	testUser, testPass := "user", "pass"
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("ETag", "123")
+		if r.Method == http.MethodHead {
+			return
+		}
+
+		called = true
+		if gotuser, gotpass, ok := r.BasicAuth(); !ok || gotuser != testUser || gotpass != testPass {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		http.FileServer(http.Dir("testdata/packages")).ServeHTTP(w, r)
+	}))
+	defer s.Close()
+
+	ctx := context.Background()
+	_, err := build.New(ctx, fs.NewMemFS(),
+		build.WithAuth("baduser", "badpass"),
+		build.WithImageConfiguration(types.ImageConfiguration{
+			Contents: types.ImageContents{
+				Keyring: []string{s.URL + "/melange.rsa.pub"},
+				// We don't even need to specify repository or packages, since keyring init will fail without auth.
+			},
+			Archs: types.ParseArchitectures([]string{"amd64", "arm64"}),
+		}),
+	)
+	require.Error(t, err, "build should have failed to init keyring")
+	require.True(t, called)
 }
