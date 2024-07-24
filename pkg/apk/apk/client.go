fe5f1fd78e9b8c94486d6b0419fa4b273524eaf4
Dan Luhring <dluhring@chainguard.dev>
2024-07-24 12:47:28 -0400

    begin a new APK client
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

---
+++ pkg/apk/apk/client.go
@@ -0,0 +1,65 @@
+package apk
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"chainguard.dev/apko/pkg/apk/auth"
+)
+
+const (
+	WolfiAPKRepo                = "https://packages.wolfi.dev/os"
+	ChainguardEnterpriseAPKRepo = "https://packages.cgr.dev/os"
+	ChainguardExtrasAPKRepo     = "https://packages.cgr.dev/extras"
+)
+
+const (
+	Aarch64Arch = "aarch64"
+	X86_64Arch  = "x86_64"
+)
+
+// Client is a client for interacting with an APK package repository.
+type Client struct {
+	httpClient *http.Client
+}
+
+// NewClient creates a new Client, suitable for accessing remote APK indexes and
+// packages.
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	return &Client{httpClient: httpClient}
+}
+
+// GetRemoteIndex retrieves the index of APK packages from the specified remote
+// repository.
+func (c Client) GetRemoteIndex(ctx context.Context, apkRepo, arch string) (*APKIndex, error) {
+	indexURL := IndexURL(apkRepo, arch)
+
+	u, err := url.Parse(indexURL)
+	if err != nil {
+		return nil, fmt.Errorf("parsing %q: %w", indexURL, err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("GET %q: %w", u.Redacted(), err)
+	}
+	if err := auth.DefaultAuthenticators.AddAuth(ctx, req); err != nil {
+		return nil, fmt.Errorf("error adding auth: %w", err)
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("GET %q: %w", u.Redacted(), err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("GET %q: status %d: %s", u.Redacted(), resp.StatusCode, resp.Status)
+	}
+
+	return IndexFromArchive(resp.Body)
+}
