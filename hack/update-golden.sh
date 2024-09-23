7cc72c23b0c828f67c5abd6820c9063dcde3c63d
Jason Hall <jason@chainguard.dev>
2024-09-23 13:29:27 -0400

    fix base image test too
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- hack/update-golden.sh
+++ hack/update-golden.sh
@@ -8,4 +8,6 @@ set -ex
 (cd internal/cli && \
   rm -rf ./testdata/golden && \
   mkdir -p ./testdata/golden/sboms && \
-  apko build --sbom-formats spdx --sbom-path ./testdata/golden/sboms ./testdata/apko.yaml golden:latest ./testdata/golden)
+  apko build --sbom-formats spdx --sbom-path ./testdata/golden/sboms ./testdata/apko.yaml golden:latest ./testdata/golden && \
+  rm -rf ./testdata/top_image && \
+  apko build ./testdata/base_image.apko.yaml top_golden:latest ./testdata/top_image)
