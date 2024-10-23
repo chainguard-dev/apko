95023c14f32abf002f890cbeaeda69502c5726de
Jason Hall <jason@chainguard.dev>
2024-10-23 11:43:46 -0400

    fix checksum and test
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/apk/apk/installed.go
+++ pkg/apk/apk/installed.go
@@ -216,7 +216,7 @@ func (a *APK) updateTriggers(pkg *Package, controlTarGz io.Reader) error {
 	}
 
 	for _, value := range values {
-		if _, err := triggers.Write([]byte(fmt.Sprintf("%s Q1%s\n", base64.StdEncoding.EncodeToString(pkg.Checksum), value))); err != nil {
+		if _, err := triggers.Write([]byte(fmt.Sprintf("Q1%s %s\n", base64.StdEncoding.EncodeToString(pkg.Checksum), value))); err != nil {
 			return fmt.Errorf("unable to write triggers file %s: %w", triggersFilePath, err)
 		}
 	}
