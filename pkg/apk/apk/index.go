4fb98303ddc772b534f559f1d9e37db33d027191
Jason Hall <jason@chainguard.dev>
2024-10-25 08:39:45 -0400

    fix data race in index cache
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/apk/apk/index.go
+++ pkg/apk/apk/index.go
@@ -149,7 +149,11 @@ func (i *indexCache) get(ctx context.Context, repoName, repoURL string, keys map
 		key := fmt.Sprintf("%s@%s", u, etag)
 
 		once, _ := i.onces.LoadOrStore(key, &sync.Once{})
+		var mu sync.Mutex
 		once.(*sync.Once).Do(func() {
+			mu.Lock()
+			defer mu.Unlock()
+
 			// If we've seen this URL before, delete any references to old indexes so we can GC them.
 			prev, ok := i.urlToEtag[u]
 			if ok {
