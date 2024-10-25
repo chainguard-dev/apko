86d90786ecd730bbc502522adc14779e167161de
Jason Hall <jason@chainguard.dev>
2024-10-25 08:55:33 -0400

    fix data race in index cache (#1369)
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/apk/apk/index.go
+++ pkg/apk/apk/index.go
@@ -61,6 +61,7 @@ type indexCache struct {
 	// For remote indexes.
 	onces     sync.Map
 	urlToEtag map[string]string
+	etagMu    sync.Mutex // guards urlToEtag
 
 	// For local indexes.
 	sync.Mutex
@@ -151,17 +152,22 @@ func (i *indexCache) get(ctx context.Context, repoName, repoURL string, keys map
 		once, _ := i.onces.LoadOrStore(key, &sync.Once{})
 		once.(*sync.Once).Do(func() {
 			// If we've seen this URL before, delete any references to old indexes so we can GC them.
+			// Lock reads/writes to the map, without blocking the fetchAndParse goroutine.
+			i.etagMu.Lock()
 			prev, ok := i.urlToEtag[u]
 			if ok {
 				prevKey := fmt.Sprintf("%s@%s", u, prev)
 				i.forget(prevKey)
 			}
+			i.etagMu.Unlock()
 
 			idx, err := fetchAndParse(etag)
 			i.store(key, idx, err)
 
 			// Record the current etag for this URL so we can GC it later.
+			i.etagMu.Lock()
 			i.urlToEtag[u] = etag
+			i.etagMu.Unlock()
 		})
 
 		return i.load(key)
