chainguard-dev/apko

commit 78cc0a5d7ee82be793ef384f3d4ab57bd7193446

Make etag checks optional (#1327)
This isn't perfect but I think it moves us closer in the right
direction. Previously, we had a process-wide global etag cache that
would prevent sending multiple HEAD requests to check the etag value of
any URLs we fetched (keys and APKINDEX, mostly).

This is great for a local tool or even the terraform provider, but it's
awful as a library used by a service because the lifetime of the process
is liable to outlive the validity of the cach.

This hoists that global etag caching mechanism out into a little
apk.Cache struct, which is where I'm hoping to put even more things that
I'm ashamed of going forward.

Using the same apk.NewCache(true) instance across multiple builds will
skip HEAD requests to the same URL and just assume that the tag hasn't
changed. This was the previous default behavior.

Using the same apk.NewCache(false) instance across multiple builds (the
default) will send a HEAD request prior to every key or index fetch in
order to see if they've changed.

We still have some global caching going on for actually parsing the
results, which keys off of etag values, so we shouldn't have any
progressions re: the recent CPU savings changes I've made.

Another thing this introduces (because we dropped the more powerful
etag-based caching) is a some singleflight instances for these outgoing
requests. We don't want to cache the results indefinitely, but we do
want to avoid sending redundant requests in every case, so
apk.NewCache(false) will still coalesce multiple concurrent requests for
the same URL, so there's still benefit to using this cache object even
with the etag caching disabled.

One thing that's missing here is a distinction between ~request-scoped
caching if e.g. I want to use an etag cache for any outgoing URLs for a
single build, but I also want to use a global cache for coalescing
requests. I'll probably split the singleflight stuff off from this

pkg/apk/apk/options.go
@@ -83,7 +83,7 @@ func WithFS(fs apkfs.FullFS) Option {
 //
 // If offline is true, only read from the cache and do not make any network requests to
 // populate it.
-func WithCache(cacheDir string, offline bool) Option {
+func WithCache(cacheDir string, offline bool, shared *Cache) Option {
 	return func(o *opts) error {
 		var err error
 		if cacheDir == "" {
@@ -96,6 +96,7 @@ func WithCache(cacheDir string, offline bool) Option {
 		o.cache = &cache{
 			dir:     cacheDir,
 			offline: offline,
+			shared:  shared,
 		}
 		return nil
 	}

pkg/build/options.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"time"
 
+	"chainguard.dev/apko/pkg/apk/apk"
 	"chainguard.dev/apko/pkg/apk/auth"
 	"chainguard.dev/apko/pkg/build/types"
 
@@ -189,11 +190,12 @@ func WithAnnotations(annotations map[string]string) Option {
 	}
 }
 
-// WithCacheDir set the cache directory to use
-func WithCacheDir(cacheDir string, offline bool) Option {
+// WithCache set the cache directory to use
+func WithCache(cacheDir string, offline bool, shared *apk.Cache) Option {
 	return func(bc *Context) error {
 		bc.o.CacheDir = cacheDir
 		bc.o.Offline = offline
+		bc.o.SharedCache = shared
 		return nil
 	}
 }

pkg/options/options.go
@@ -21,6 +21,7 @@ import (
 	"runtime"
 	"time"
 
+	"chainguard.dev/apko/pkg/apk/apk"
 	"chainguard.dev/apko/pkg/apk/auth"
 	"chainguard.dev/apko/pkg/build/types"
 )
@@ -49,6 +50,7 @@ type Options struct {
 	Local                   bool               `json:"local,omitempty"`
 	CacheDir                string             `json:"cacheDir,omitempty"`
 	Offline                 bool               `json:"offline,omitempty"`
+	SharedCache             *apk.Cache         `json:"-"`
 	Lockfile                string             `json:"lockfile,omitempty"`
 	Auth                    auth.Authenticator `json:"-"`
 	IncludePaths            []string           `json:"includePaths,omitempty"`
@@ -61,6 +63,7 @@ var Default = Options{
 	Arch:            types.ParseArchitecture(runtime.GOARCH),
 	SourceDateEpoch: time.Unix(0, 0).UTC(),
 	Auth:            auth.DefaultAuthenticators,
+	SharedCache:     apk.NewCache(false),
 }
 
 // Tempdir returns the temporary directory where apko will create

internal/cli/build.go
@@ -33,6 +33,7 @@ import (
 
 	"github.com/chainguard-dev/clog"
 
+	"chainguard.dev/apko/pkg/apk/apk"
 	"chainguard.dev/apko/pkg/build"
 	"chainguard.dev/apko/pkg/build/oci"
 	"chainguard.dev/apko/pkg/build/types"
@@ -108,7 +109,7 @@ Along the image, apko will generate SBOMs (software bill of materials) describin
 				build.WithTags(args[1]),
 				build.WithVCS(withVCS),
 				build.WithAnnotations(annotations),
-				build.WithCacheDir(cacheDir, offline),
+				build.WithCache(cacheDir, offline, apk.NewCache(true)),
 				build.WithLockFile(lockfile),
 				build.WithTempDir(tmp),
 				build.WithIncludePaths(includePaths),

internal/cli/dot.go
@@ -72,7 +72,7 @@ apko dot --web -S example.yaml
 				build.WithExtraKeys(extraKeys),
 				build.WithExtraBuildRepos(extraBuildRepos),
 				build.WithExtraRuntimeRepos(extraRuntimeRepos),
-				build.WithCacheDir(cacheDir, offline),
+				build.WithCache(cacheDir, offline, apk.NewCache(true)),
 			)
 		},
 	}

internal/cli/publish.go
@@ -31,6 +31,7 @@ import (
 
 	"github.com/chainguard-dev/clog"
 
+	"chainguard.dev/apko/pkg/apk/apk"
 	"chainguard.dev/apko/pkg/build"
 	"chainguard.dev/apko/pkg/build/oci"
 	"chainguard.dev/apko/pkg/build/types"
@@ -116,7 +117,7 @@ in a keychain.`,
 					build.WithTags(args[1:]...),
 					build.WithVCS(withVCS),
 					build.WithAnnotations(annotations),
-					build.WithCacheDir(cacheDir, offline),
+					build.WithCache(cacheDir, offline, apk.NewCache(true)),
 					build.WithLockFile(lockfile),
 					build.WithTempDir(tmp),
 					build.WithIgnoreSignatures(ignoreSignatures),

internal/cli/show-config.go
@@ -23,6 +23,7 @@ import (
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 
+	"chainguard.dev/apko/pkg/apk/apk"
 	apkfs "chainguard.dev/apko/pkg/apk/fs"
 	"chainguard.dev/apko/pkg/build"
 )
@@ -49,7 +50,7 @@ The derived configuration is rendered in YAML.
 				build.WithExtraKeys(extraKeys),
 				build.WithExtraBuildRepos(extraBuildRepos),
 				build.WithExtraRuntimeRepos(extraRuntimeRepos),
-				build.WithCacheDir(cacheDir, offline),
+				build.WithCache(cacheDir, offline, apk.NewCache(true)),
 			)
 		},
 	}

internal/cli/show-packages.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/chainguard-dev/clog"
 
+	"chainguard.dev/apko/pkg/apk/apk"
 	"chainguard.dev/apko/pkg/build"
 	"chainguard.dev/apko/pkg/build/types"
 )
@@ -108,7 +109,7 @@ packagelock and packagelock-source are particularly useful for inserting back in
 				build.WithExtraKeys(extraKeys),
 				build.WithExtraBuildRepos(extraBuildRepos),
 				build.WithExtraRuntimeRepos(extraRuntimeRepos),
-				build.WithCacheDir(cacheDir, offline),
+				build.WithCache(cacheDir, offline, apk.NewCache(true)),
 			)
 		},
 	}

commit 818ff3c8202feafb6ed8032961b1a6f11fa78132

fix: --cache-dir broken after in 0.19.3+ (#1382)
Resolves #1378 where `--cache-dir` was broken after in 0.19.3+ due to a
regression in #1327. This error happened because `cacheDir` is not an
absolute path when provided via the CLI option. This PR fixes the bug by
resolving `cacheDir` to an absolute path when initializing the `cache`
instance.

Signed-off-by: Luke Young <91491244+lyoung-confluent@users.noreply.github.com>

pkg/apk/apk/options.go
@@ -92,6 +92,11 @@ func WithCache(cacheDir string, offline bool, shared *Cache) Option {
 				return err
 			}
 			cacheDir = filepath.Join(cacheDir, "dev.chainguard.go-apk")
+		} else {
+			cacheDir, err = filepath.Abs(cacheDir)
+			if err != nil {
+				return err
+			}
 		}
 		o.cache = &cache{
 			dir:     cacheDir,