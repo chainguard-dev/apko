9ca37045a7696e9ff74be13d76f2d920d740e64c
Jon Johnson <jon.johnson@chainguard.dev>
2025-04-10 15:22:01 -0700

    Rewrite writeTar in terms of iterators
    Instead of hard-coding writeTar to only be able to write to a single
    destination, this splits most of the logic out into a walkFS iterator
    that yields files to the caller.
    
    For writeTar, this change is a no-op, but I'll be trying to write to
    multiple layers simultaneously in a coming change, and that will be very
    messy to do deep inside this walking code, so turning it into an
    iterators will make it much easier to read.
    
    I also noticed that this package import accidentally changed from
    pkg/apk/passwd to pkg/passwd. These packages are identical, so I deleted
    the other one.
    
    Signed-off-by: Jon Johnson <jon.johnson@chainguard.dev>

--- pkg/build/tarball.go
+++ pkg/build/tarball.go
@@ -20,13 +20,14 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"iter"
 	"os"
 
 	"go.opentelemetry.io/otel"
 	"golang.org/x/sys/unix"
 
 	apkfs "chainguard.dev/apko/pkg/apk/fs"
-	"chainguard.dev/apko/pkg/apk/passwd"
+	"chainguard.dev/apko/pkg/passwd"
 )
 
 const xattrTarPAXRecordsPrefix = "SCHILY.xattr."
@@ -34,110 +35,22 @@ const xattrTarPAXRecordsPrefix = "SCHILY.xattr."
 // writeTar writes a tarball to the provided io.Writer from the provided fs.FS.
 // The etc/passwd and etc/group file provide username and group name mappings for the tar.
 func writeTar(ctx context.Context, dst io.Writer, fsys apkfs.FullFS) error { //nolint:gocyclo
-	ctx, span := otel.Tracer("go-apk").Start(ctx, "WriteTar")
+	ctx, span := otel.Tracer("go-apk").Start(ctx, "writeTar")
 	defer span.End()
 
 	tw := tar.NewWriter(dst)
-
-	// get the uname and gname maps
-	usersFile, _ := passwd.ReadUserFile(fsys, "etc/passwd")
-	groupsFile, _ := passwd.ReadGroupFile(fsys, "etc/group")
-	users := map[int]string{}
-	groups := map[int]string{}
-	for _, u := range usersFile.Entries {
-		users[int(u.UID)] = u.UserName
-	}
-	for _, g := range groupsFile.Entries {
-		groups[int(g.GID)] = g.GroupName
-	}
-
 	buf := make([]byte, 1<<20)
 
-	if err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
-		if err := ctx.Err(); err != nil {
-			return err
-		}
-		// skip the root path, superfluous
-		if path == "." {
-			return nil
-		}
-
+	for f, err := range walkFS(ctx, fsys) {
 		if err != nil {
 			return err
 		}
-
-		info, err := d.Info()
-		if err != nil {
+		if err := tw.WriteHeader(f.header); err != nil {
 			return err
 		}
 
-		var (
-			link         string
-			major, minor uint32
-			isCharDevice bool
-		)
-		if info.Mode()&os.ModeSymlink == os.ModeSymlink {
-			if link, err = fsys.Readlink(path); err != nil {
-				return err
-			}
-		}
-
-		if info.Mode()&os.ModeCharDevice == os.ModeCharDevice {
-			isCharDevice = true
-			dev, err := fsys.Readnod(path)
-			if err != nil {
-				return err
-			}
-			major = unix.Major(uint64(dev))
-			minor = unix.Minor(uint64(dev))
-		}
-
-		header, err := tar.FileInfoHeader(info, link)
-		if err != nil {
-			return err
-		}
-		// devices
-		if isCharDevice {
-			header.Devmajor = int64(major)
-			header.Devminor = int64(minor)
-		}
-		// work around some weirdness, without this we wind up with just the basename
-		header.Name = path
-
-		header.ModTime = info.ModTime()
-
-		if name, ok := users[header.Uid]; ok {
-			header.Uname = name
-		}
-		if name, ok := groups[header.Gid]; ok {
-			header.Gname = name
-		}
-
-		if link != "" {
-			header.Typeflag = tar.TypeSymlink
-		}
-
-		if header.PAXRecords == nil {
-			header.PAXRecords = map[string]string{}
-		}
-
-		// only capture xattrs for real objects in the FS
-		if header.Typeflag == tar.TypeReg || header.Typeflag == tar.TypeDir {
-			xattrs, err := fsys.ListXattrs(path)
-			// we can ignore errors
-			if err == nil && xattrs != nil {
-				for name, value := range xattrs {
-					header.PAXRecords[xattrTarPAXRecordsPrefix+name] = string(value)
-				}
-			}
-		}
-
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-
-		if info.Mode().IsRegular() && header.Size > 0 {
-			data, err := fsys.Open(path)
+		if f.info.Mode().IsRegular() && f.header.Size > 0 {
+			data, err := fsys.Open(f.path)
 			if err != nil {
 				return err
 			}
@@ -147,15 +60,130 @@ func writeTar(ctx context.Context, dst io.Writer, fsys apkfs.FullFS) error { //n
 				return err
 			}
 		}
-
-		return nil
-	}); err != nil {
-		return fmt.Errorf("writing tar archive: %w", err)
 	}
 
 	if err := tw.Close(); err != nil {
 		return fmt.Errorf("closing tar writer: %w", err)
 	}
-
 	return nil
+}
+
+// file holds all the info we need to yield from walkFS.
+//
+// We need both Header and info because the fs.DirEntry.Info()
+// implementation is controlled by our tarfs implementation,
+// and we use that as a sidechannel to propagate package ownership.
+type file struct {
+	path   string
+	info   fs.FileInfo
+	header *tar.Header
+}
+
+func walkFS(ctx context.Context, fsys apkfs.FullFS) iter.Seq2[*file, error] {
+	return func(yield func(*file, error) bool) {
+		usersFile, _ := passwd.ReadUserFile(fsys, "etc/passwd")
+		groupsFile, _ := passwd.ReadGroupFile(fsys, "etc/group")
+		users := map[int]string{}
+		groups := map[int]string{}
+		for _, u := range usersFile.Entries {
+			users[int(u.UID)] = u.UserName
+		}
+		for _, g := range groupsFile.Entries {
+			groups[int(g.GID)] = g.GroupName
+		}
+
+		if err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+			if err := ctx.Err(); err != nil {
+				return err
+			}
+			// skip the root path, superfluous
+			if path == "." {
+				return nil
+			}
+
+			if err != nil {
+				return err
+			}
+
+			info, err := d.Info()
+			if err != nil {
+				return err
+			}
+
+			var link string
+			if info.Mode()&os.ModeSymlink == os.ModeSymlink {
+				if link, err = fsys.Readlink(path); err != nil {
+					return err
+				}
+			}
+
+			header, err := tar.FileInfoHeader(info, link)
+			if err != nil {
+				return err
+			}
+
+			if info.Mode()&os.ModeCharDevice == os.ModeCharDevice {
+				dev, err := fsys.Readnod(path)
+				if err != nil {
+					return err
+				}
+				header.Devmajor = int64(unix.Major(uint64(dev)))
+				header.Devminor = int64(unix.Minor(uint64(dev)))
+			}
+
+			// tar.FileInfoHeader sets Name to the base name of the file,
+			// which is weird, because tar.Header.Name is the full path.
+			// In some cases, that does make sense, because fs.FileInfo.Name()
+			// is only the basename, but in tar.FileInfoHeader, it populates
+			// many of the fields based on the fs.FileInfo.Sys() method returning
+			// a tar.Header, which _does_ have the full path!
+			//
+			// Regardless, we want to override the name with the full path, here,
+			// because this path represents the full path _as written into this fs_,
+			// which might be different from the original tar.Header's full path.
+			header.Name = path
+
+			header.ModTime = info.ModTime()
+
+			if name, ok := users[header.Uid]; ok {
+				header.Uname = name
+			}
+			if name, ok := groups[header.Gid]; ok {
+				header.Gname = name
+			}
+
+			if link != "" {
+				header.Typeflag = tar.TypeSymlink
+			}
+
+			if header.PAXRecords == nil {
+				header.PAXRecords = map[string]string{}
+			}
+
+			// only capture xattrs for real objects in the FS
+			if header.Typeflag == tar.TypeReg || header.Typeflag == tar.TypeDir {
+				xattrs, err := fsys.ListXattrs(path)
+				// we can ignore errors
+				if err == nil && xattrs != nil {
+					for name, value := range xattrs {
+						header.PAXRecords[xattrTarPAXRecordsPrefix+name] = string(value)
+					}
+				}
+			}
+
+			if !yield(&file{
+				path:   path,
+				info:   info,
+				header: header,
+			}, nil) {
+				return fs.SkipAll
+			}
+
+			return nil
+		}); err != nil {
+			if !yield(nil, err) {
+				return
+			}
+		}
+	}
 }
