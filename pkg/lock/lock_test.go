chainguard-dev/apko

commit e85ed818320f793afaff25b3e1b691f5b966be39

Make apko hermetic again. (#1654)
Apko started to ignore lock files, i.e. even if the lock_file to `apko
build` is specified explicitly, the configuration resolution was
calling: `LockImageConfiguration() -> BuildPackageList() ->
ResolveWorld()` thus
depending on the current (up to date) state of the `ApkIndex`.

This PR makes sure that list of packages defined in the lock file takes
precedence (blocks) reevaluation of the list of packages to install.

pkg/build/lock.go
@@ -26,6 +26,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 
 	"chainguard.dev/apko/pkg/build/types"
+	pkglock "chainguard.dev/apko/pkg/lock"
 )
 
 // LockImageConfiguration returns a map of locked image configurations for each architecture,
@@ -48,17 +49,62 @@ func LockImageConfiguration(ctx context.Context, ic types.ImageConfiguration, op
 		return nil, nil, err
 	}
 
-	archs := make([]resolved, 0, len(input.Archs))
-	ics := make(map[string]*types.ImageConfiguration, len(input.Archs)+1)
-
 	// Determine the exact versions of our transitive packages and lock them
 	// down in the "resolved" configuration, so that this build may be
 	// reproduced exactly.
-	toInstalls, err := mc.BuildPackageLists(ctx)
-	if err != nil {
-		return nil, nil, err
+	var pls map[string][]string
+	missing := map[string][]string{}
+	if o.Lockfile == "" {
+		archs, err := resolvePackageList(ctx, mc)
+		if err != nil {
+			return nil, nil, err
+		}
+		pls, missing, err = unify(input.Contents.Packages, archs)
+		if err != nil {
+			return nil, missing, err
+		}
+	} else {
+		l, err := pkglock.FromFile(o.Lockfile)
+		if err != nil {
+			return nil, nil, err
+		}
+		for _, bc := range mc.Contexts {
+			if err := bc.VerifyLockfileConsistency(ctx, l.Config); err != nil {
+				return nil, nil, err
+			}
+		}
+		pls = l.Arch2LockedPackages()
 	}
 
+	ics := make(map[string]*types.ImageConfiguration, len(mc.Contexts)+1)
+	// Set the locked package lists.
+	for arch, pl := range pls {
+		// Create a defensive copy of "input".
+		copied := types.ImageConfiguration{}
+		if err := input.MergeInto(&copied); err != nil {
+			return nil, nil, err
+		}
+
+		copied.Contents.Packages = pl
+
+		if arch != "index" {
+			// Overwrite single-arch configs with their specific arch.
+			copied.Archs = []types.Architecture{types.ParseArchitecture(arch)}
+		}
+
+		ics[arch] = &copied
+	}
+
+	return ics, missing, nil
+}
+
+func resolvePackageList(ctx context.Context, mc *MultiArch) ([]resolved, error) {
+	archs := make([]resolved, 0, len(mc.Contexts))
+
+	toInstalls, err := mc.BuildPackageLists(ctx)
+	if err != nil {
+		return nil, err
+	}
 	for arch, pkgs := range toInstalls {
 		r := resolved{
 			// ParseArchitecture normalizes the architecture into the
@@ -87,31 +133,7 @@ func LockImageConfiguration(ctx context.Context, ic types.ImageConfiguration, op
 		}
 		archs = append(archs, r)
 	}
-
-	pls, missing, err := unify(input.Contents.Packages, archs)
-	if err != nil {
-		return nil, missing, err
-	}
-
-	// Set the locked package lists.
-	for arch, pl := range pls {
-		// Create a defensive copy of "input".
-		copied := types.ImageConfiguration{}
-		if err := input.MergeInto(&copied); err != nil {
-			return nil, nil, err
-		}
-
-		copied.Contents.Packages = pl
-
-		if arch != "index" {
-			// Overwrite single-arch configs with their specific arch.
-			copied.Archs = []types.Architecture{types.ParseArchitecture(arch)}
-		}
-
-		ics[arch] = &copied
-	}
-
-	return ics, missing, nil
+	return archs, nil
 }
 
 type resolved struct {

pkg/lock/lock.go
@@ -78,3 +78,16 @@ func (lock Lock) SaveToFile(lockFile string) error {
 	// #nosec G306 -- apk world must be publicly readable
 	return os.WriteFile(lockFile, jsonb, os.ModePerm)
 }
+
+// Arch2LockedPackages returns map: for each arch -> list of {package_name}={version}.
+func (lock Lock) Arch2LockedPackages() map[string][]string {
+	lockedPackages := map[string][]string{}
+	for _, p := range lock.Contents.Packages {
+		_, ok := lockedPackages[p.Architecture]
+		if !ok {
+			lockedPackages[p.Architecture] = []string{}
+		}
+		lockedPackages[p.Architecture] = append(lockedPackages[p.Architecture], fmt.Sprintf("%s=%s", p.Name, p.Version))
+	}
+	return lockedPackages
+}

internal/cli/testdata/top_image/blobs/sha256/14160a36b6fb2f3d325eb09ada490e6ca171285b2ab0cd5d41989c11972eb153
@@ -1,0 +1,1 @@
+{"schemaVersion":2,"mediaType":"application/vnd.oci.image.manifest.v1+json","config":{"mediaType":"application/vnd.oci.image.config.v1+json","size":785,"digest":"sha256:c167d5b680a5a084c621b7e9af9973d68ccf6475af4b0858f2067fb556614f3b"},"layers":[{"mediaType":"application/vnd.oci.image.layer.v1.tar+gzip","size":4126,"digest":"sha256:bf74ddaf55d32ec9672a0a40efc6cb1bf0a167763c18fc22586c8a301167822f"},{"mediaType":"application/vnd.oci.image.layer.v1.tar+gzip","size":2885,"digest":"sha256:81168b5de29746299ae4cdb269544f6dff75d8f0e6b03b314cee06723c7e2f6b"}],"annotations":{"org.opencontainers.image.created":"1970-01-01T00:00:00Z"}}

internal/cli/testdata/top_image/blobs/sha256/45338ea1681a61ef1ff24b109f0c13aa95113a33cfb684377f8b89fe63b754b5
@@ -1,0 +1,1 @@
+{"schemaVersion":2,"mediaType":"application/vnd.oci.image.manifest.v1+json","config":{"mediaType":"application/vnd.oci.image.config.v1+json","size":785,"digest":"sha256:bafb997874b76dcef4ccfbf7d651aaab7ec557ce580053743664d5f7294de0e7"},"layers":[{"mediaType":"application/vnd.oci.image.layer.v1.tar+gzip","size":4123,"digest":"sha256:583625b6164fff3b017f62b9fcd60cb53fff18a7e89ee538212134a13fc29fb1"},{"mediaType":"application/vnd.oci.image.layer.v1.tar+gzip","size":2886,"digest":"sha256:10a1a18309374068005a73edacbd06b17fe67378c95d1e66e0cc2be1270c0328"}],"annotations":{"org.opencontainers.image.created":"1970-01-01T00:00:00Z"}}

internal/cli/testdata/top_image/blobs/sha256/8db8a41dec72567ec9f13fa97aea9fbf53ac9b0cde4dc939e955c0da6e4bad1d
@@ -1,1 +1,0 @@
-{"architecture":"arm64","author":"github.com/chainguard-dev/apko","created":"1970-01-01T00:00:00Z","history":[{"author":"apko","created":"1970-01-01T00:00:00Z","created_by":"apko","comment":"This is an apko single-layer image"},{"author":"apko","created":"1970-01-01T00:00:00Z","created_by":"apko","comment":"This is an apko single-layer image"}],"os":"linux","rootfs":{"type":"layers","diff_ids":["sha256:2888aac57b90cf66093aa48092bf1f1f1b1bdb85bde8601a5f8cf0f06c814763","sha256:1cbbe282254fee45dafc3732bb66f64a4402603bf579496ab55ed8327d92f105"]},"config":{"Entrypoint":["/bin/sh","-l"],"Env":["PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin","SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt"],"Labels":{"org.opencontainers.image.created":"1970-01-01T00:00:00Z"}}}

commit 7a6735200c242efdcc330748e3acdfb9dc001e67

Respect arch flags when using lockfiles
This ~regressed in https://github.com/chainguard-dev/apko/pull/1654

Signed-off-by: Jon Johnson <jon.johnson@chainguard.dev>

pkg/build/lock.go
@@ -73,7 +73,7 @@ func LockImageConfiguration(ctx context.Context, ic types.ImageConfiguration, op
 				return nil, nil, err
 			}
 		}
-		pls = l.Arch2LockedPackages()
+		pls = l.Arch2LockedPackages(input.Archs)
 	}
 
 	ics := make(map[string]*types.ImageConfiguration, len(mc.Contexts)+1)

pkg/lock/lock.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"chainguard.dev/apko/pkg/build/types"
 )
 
 type Lock struct {
@@ -79,12 +81,21 @@ func (lock Lock) SaveToFile(lockFile string) error {
 	return os.WriteFile(lockFile, jsonb, os.ModePerm)
 }
 
-// Arch2LockedPackages returns map: for each arch -> list of {package_name}={version}.
-func (lock Lock) Arch2LockedPackages() map[string][]string {
+// Arch2LockedPackages returns map: for each arch -> list of {package_name}={version} in archs.
+func (lock Lock) Arch2LockedPackages(archs []types.Architecture) map[string][]string {
+	want := map[string]struct{}{}
+	for _, arch := range archs {
+		want[arch.String()] = struct{}{}
+	}
 	lockedPackages := map[string][]string{}
 	for _, p := range lock.Contents.Packages {
 		_, ok := lockedPackages[p.Architecture]
 		if !ok {
+			if _, ok := want[types.Architecture(p.Architecture).String()]; ok {
+				// Ignore architectures we aren't building.
+				continue
+			}
+
 			lockedPackages[p.Architecture] = []string{}
 		}
 		lockedPackages[p.Architecture] = append(lockedPackages[p.Architecture], fmt.Sprintf("%s=%s", p.Name, p.Version))

pkg/lock/lock_test.go
@@ -1,0 +1,42 @@
+// Copyright 2025 Chainguard, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lock
+
+import (
+	"testing"
+
+	"chainguard.dev/apko/pkg/build/types"
+)
+
+func TestArch2LockedPackages(t *testing.T) {
+	l := Lock{
+		Contents: LockContents{
+			Packages: []LockPkg{{
+				Name:         "avx",
+				Version:      "1.2.3",
+				Architecture: "x86_64",
+			}, {
+				Name:         "sve",
+				Version:      "2.3.4",
+				Architecture: "aarch64",
+			}},
+		},
+	}
+
+	archs := []types.Architecture{types.Architecture("aarch64")}
+	if got, want := len(l.Arch2LockedPackages(archs)), 1; got != want {
+		t.Errorf("wanted %d arch, got %d", want, got)
+	}
+}