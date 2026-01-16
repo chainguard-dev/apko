c53f978883cbbf302d2d4794511a40b3a1176e01
Markus Thömmes <markus.thoemmes@chainguard.dev>
2026-01-16 18:31:28 +0100

    Fix retry handling of the range retry transport (#2025)
    Before a recent refactor in 89f7c136729d533c74518b174099a1131d5092a9 the
    "transport" used an HTTP inside of it, which dealt with redirects inside
    of the transport itself. This is now subtly broken in that the default
    usage in apko passes a cache in, which in term uses a client as well. If
    this transport is used without a client though, redirects actually fail.
    
    This makes it so that redirects are passed through as is so an external
    client can handle them accordingly.

--- pkg/apk/apk/transport_test.go
+++ pkg/apk/apk/transport_test.go
@@ -110,7 +110,12 @@ func TestTransport(t *testing.T) {
 		StatusCode: http.StatusInternalServerError,
 	}
 
-	for i, tc := range []struct {
+	redirect := &http.Response{
+		StatusCode: http.StatusSeeOther,
+		Header:     map[string][]string{"Location": {"foobar"}},
+	}
+
+	for _, tc := range []struct {
 		name    string
 		readers []io.Reader
 		resps   []*http.Response
@@ -155,9 +160,14 @@ func TestTransport(t *testing.T) {
 		resps:   []*http.Response{ok(2), ok(2)}, //nolint:bodyclose
 		ranges:  []int{0, size},
 		want:    mr(cr(), cr()),
+	}, {
+		name:    "redirect response from server",
+		readers: []io.Reader{http.NoBody, mr(cr(), cr())},
+		resps:   []*http.Response{redirect, ok(2)}, //nolint:bodyclose
+		ranges:  []int{0, 0, size},
+		want:    mr(cr(), cr()),
 	}} {
-		name := fmt.Sprintf("[%d]", i)
-		t.Run(name, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			tr := &testReader{tc.readers, 0}
 			tt := &testTransport{
 				rc:     tr,
@@ -165,14 +175,16 @@ func TestTransport(t *testing.T) {
 				ranges: tc.ranges,
 			}
 
-			rt := NewRangeRetryTransport(tt)
+			client := &http.Client{
+				Transport: NewRangeRetryTransport(tt),
+			}
 
 			req := &http.Request{
 				URL:    &url.URL{},
 				Header: map[string][]string{},
 			}
 
-			resp, err := rt.RoundTrip(req)
+			resp, err := client.Do(req)
 			if err != nil {
 				t.Fatal(err)
 			}
