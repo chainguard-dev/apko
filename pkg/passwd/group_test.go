9465ca988cb1bffd901dcf6f0edc15bd576bc9db
Steve Beattie <steve.beattie@chainguard.dev>
2026-01-30 16:56:31 -0800

    tests: improve passwd/group parsing tests
    The tests that are supposed to validate that existing passwd and group
    files are being parsed correctly didn't actually verify that any
    elements were successfully returned from the parse operation. This meant
    that an exploratory patch that converted apkofs.DirFS() to tarfs()
    looked like it succeeded but actually didn't.
    
    Fix this by ensuring that the checked for entries are actually found and
    also expand the fields checked in the passwd and group dbs.
    
    Signed-off-by: Steve Beattie <steve.beattie@chainguard.dev>

--- pkg/passwd/group_test.go
+++ pkg/passwd/group_test.go
@@ -1,4 +1,4 @@
-// Copyright 2022, 2023 Chainguard, Inc.
+// Copyright 2022-2026 Chainguard, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	apkfs "chainguard.dev/apko/pkg/apk/fs"
@@ -28,15 +29,40 @@ func TestGroupParser(t *testing.T) {
 	gf, err := ReadOrCreateGroupFile(fsys, "group")
 	require.NoError(t, err)
 
+	require.NotEmpty(t, gf, "group file in existing testdata file should not be empty")
+
+	found_root := false
+	found_daemon := false
+	found_nobody := false
 	for _, ge := range gf.Entries {
 		if ge.GID == 0 {
-			require.Equal(t, "root", ge.GroupName, "gid 0 is not root")
+			assert.Equal(t, "root", ge.GroupName, "gid 0 is not root")
+			assert.Equal(t, "x", ge.Password, "gid 0 password entry is not set to x")
+			assert.Equal(t, []string{"root"}, ge.Members, "gid 0 members should just be the root user")
+			found_root = true
+		}
+
+		if ge.GID == 2 {
+			assert.Equal(t, "daemon", ge.GroupName, "gid 2 is not daemon")
+			assert.Equal(t, "x", ge.Password, "gid 2 password entry is not set to x")
+			assert.ElementsMatch(t, []string{"root", "bin", "daemon"}, ge.Members, "gid 2 members should contain root, bin, and daemon users")
+			found_daemon = true
 		}
 
 		if ge.GID == 65534 {
-			require.Equal(t, "nobody", ge.GroupName, "gid 65534 is not nobody")
+			assert.Equal(t, "nobody", ge.GroupName, "gid 65534 is not nobody")
+			assert.Equal(t, "x", ge.Password, "gid 65534 password entry is not set to x")
+			// XXX if there's no users listed as group members, Parse
+			// returns a list with one empty string as a result, not
+			// sure if that's intended behavior.
+			assert.Equal(t, []string{""}, ge.Members, "gid 65534 members should be empty")
+			found_nobody = true
 		}
 	}
+
+	assert.True(t, found_root, "group file should contain the root group")
+	assert.True(t, found_daemon, "group file should contain the daemon group")
+	assert.True(t, found_nobody, "group file should contain the nobody group")
 }
 
 func TestGroupWriter(t *testing.T) {
